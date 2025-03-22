a519d35aea793536245e50a6ab7d9921e0b94c1c
ThatGuySam <sam@sam.lc>
2025-03-22 14:45:37 -0500

    update: extend timeout to 10s

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
  
 export default defineConfig({
   test: {
+    testTimeout: 10_000,
     globalSetup: './test/global-setup.ts',
     setupFiles: [
         './test/test-setup.ts'
