2924f4ccf773db734424ee6c91bd37d74e787db4
ThatGuySam <sam@sam.lc>
2025-03-22 14:45:27 -0500

    update: use dotenv in tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
+import { config } from "dotenv";
 import { defineConfig } from 'vitest/config'
  
 export default defineConfig({
   test: {
     globalSetup: './test/global-setup.ts',
-    setupFiles: ['./test/test-setup.ts'],
+    setupFiles: [
+        './test/test-setup.ts'
+    ],
+    env: {
+        ...config({ path: ".env.development" }).parsed,
+    },
   },
 })
