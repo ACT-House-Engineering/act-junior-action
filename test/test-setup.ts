c05d8a09d2ae88772ed673981e196e9a3b4af8a4
ThatGuySam <sam@sam.lc>
2025-03-22 14:44:54 -0500

    update: disable storage testing for now

--- test/test-setup.ts
+++ test/test-setup.ts
@@ -1,8 +1,14 @@
+/**
+ * Mastra vitest setup
+ * https://mastra.ai/docs/evals/03-running-in-ci#storage-configuration
+ */
 import { beforeAll } from 'vitest';
 import { attachListeners } from '@mastra/evals';
-import { mastra } from '../src/mastra';
+// import { mastra } from '../src/mastra';
 
  
 beforeAll(async () => {
-  await attachListeners( mastra );
+    await attachListeners(
+        // mastra
+    );
 });
