55c2cc58ce8bce4645f3ae175aba3afce129b9ef
ThatGuySam <sam@sam.lc>
2025-03-22 14:49:23 -0500

    update: add Weather test

---
+++ src/mastra/agents/index.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest';
+import { evaluate } from "@mastra/evals";
+import { ToneConsistencyMetric } from "@mastra/evals/nlp";
+
+import { weatherAgent } from './index';
+import { isPersonalDevice } from '../../utils';
+
+describe('Weather Agent', () => {
+  it.skipIf(isPersonalDevice())('should validate tone consistency', async () => {
+    const metric = new ToneConsistencyMetric();
+    const result = await evaluate(weatherAgent, 'London', metric)
+ 
+    expect(result.score).toBeGreaterThan(0.9);
+  });
+});
