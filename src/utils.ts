f80ca45ede300e28476b9c2b429f5e95bf9438cf
ThatGuySam <sam@sam.lc>
2025-03-22 14:48:28 -0500

    update: add isPersonalDevice util

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+import { isMacOS, provider } from "std-env";
+
+export function isPersonalDevice() {
+    // False if running in GitHub Actions
+    if (provider === 'github_actions') {
+        return false;
+    } 
+
+    return isMacOS
+}
